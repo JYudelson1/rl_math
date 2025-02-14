277560304365886d9711b58ece970ea832ed4d60
JYudelson1 <jyudel@gmail.com>
2025-02-13 16:10:26 -0800

    no temp

--- full_runner.sh
+++ full_runner.sh
@@ -1,4 +1,4 @@
 uv run ray stop && 
 uv lock --upgrade-package openrlhf && 
-VLLM_CONFIGURE_LOGGING=0 uv run ray start --head --port 6380 --dashboard-port 8265 --num-gpus 8 --num-cpus 128 --dashboard-agent-listen-port 52366 --ray-client-server-port 9999 --temp-dir /data1/joey/tmp && 
+VLLM_CONFIGURE_LOGGING=0 uv run ray start --head --port 6380 --dashboard-port 8265 --num-gpus 8 --num-cpus 128 --dashboard-agent-listen-port 52366 --ray-client-server-port 9999 && 
 ./openrlhf_ppo.sh
